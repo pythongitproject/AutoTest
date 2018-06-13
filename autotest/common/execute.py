795e0eda5d2b6b4d79df0255fdc62cf05ca28988
linweili <295287765@qq.com>
2018-06-13 18:19:40 +0800

    execute1.2

--- autotest/common/execute.py
+++ autotest/common/execute.py
@@ -56,6 +56,7 @@ class Execute():
     #     # 签名
     #     if interface.is_sign:
     #         if_dict["body"] = get_sign(self.sign_type, if_dict["body"], self.private_key)
+
         if_dict["url"] = self.env_url + interface.url
         if_dict["if_id"] = if_id
         if_dict["if_name"] = step_content["if_name"]
@@ -82,27 +83,27 @@ class Execute():
     #     return if_dict
 
     # 验证结果
-    def validators_result(self, validators_list, res):
-        msg = ""
-        result = ""
-        for var_field in validators_list:
-            check_filed = var_field["check"]
-            expect_filed = var_field["expect"]
-            check_filed_value = self.get_param(check_filed, res)
-            if check_filed_value == expect_filed:
-                result = "pass"
-                msg = ""
-            else:
-                result = "fail"
-                msg = "字段: " + check_filed + " 实际值为：" + str(check_filed_value) + " 与期望值：" + expect_filed + " 不符"
-                break
-        return result, msg
+    # def validators_result(self, validators_list, res):
+    #     msg = ""
+    #     result = ""
+    #     for var_field in validators_list:
+    #         check_filed = var_field["check"]
+    #         expect_filed = var_field["expect"]
+    #         check_filed_value = self.get_param(check_filed, res)
+    #         if check_filed_value == expect_filed:
+    #             result = "pass"
+    #             msg = ""
+    #         else:
+    #             result = "fail"
+    #             msg = "字段: " + check_filed + " 实际值为：" + str(check_filed_value) + " 与期望值：" + expect_filed + " 不符"
+    #             break
+    #     return result, msg
 
     # 在response中提取参数, 并放到列表中
-    def get_extract(self, extract_dict, res):
-        for key, value in extract_dict.items():
-            key_value = self.get_param(key, res)
-            self.extract_dict[key] = key_value
+    # def get_extract(self, extract_dict, res):
+    #     for key, value in extract_dict.items():
+    #         key_value = self.get_param(key, res)
+    #         self.extract_dict[key] = key_value
     #
     #
     #
@@ -117,65 +118,64 @@ class Execute():
     #
 
     # 从内容中提取所有变量名, 变量格式为$variable,返回变量名list
-    def extract_variables(self, content):
-        variable_regexp = r"\$([\w_]+)"
-        if not isinstance(content, str):
-            content = str(content)
-        try:
-            return re.findall(variable_regexp, content)
-        except TypeError:
-            return []
+    # def extract_variables(self, content):
+    #     variable_regexp = r"\$([\w_]+)"
+    #     if not isinstance(content, str):
+    #         content = str(content)
+    #     try:
+    #         return re.findall(variable_regexp, content)
+    #     except TypeError:
+    #         return []
 
     # 在内容中获取某一参数的值
-    def get_param(self, param, content):
-        param_val = None
-        if isinstance(content, str):
-            # content = json.loads(content)
-            try:
-                content = json.loads(content)
-            except:
-                content = ""
-        if isinstance(content, dict):
-            param_val = self.get_param_reponse(param, content)
-        if isinstance(content, list):
-            dict_data = {}
-            for i in range(len(content)):
-                try:
-                    dict_data[str(i)] = eval(content[i])
-                except:
-                    dict_data[str(i)] = content[i]
-            param_val = self.get_param_reponse(param, dict_data)
-        if param_val is None:
-            return param_val
-        else:
-            if "$" + param == param_val:
-                param_val = None
-            return param_val
+    # def get_param(self, param, content):
+    #     param_val = None
+    #     if isinstance(content, str):
+    #         # content = json.loads(content)
+    #         try:
+    #             content = json.loads(content)
+    #         except:
+    #             content = ""
+    #     if isinstance(content, dict):
+    #         param_val = self.get_param_reponse(param, content)
+    #     if isinstance(content, list):
+    #         dict_data = {}
+    #         for i in range(len(content)):
+    #             try:
+    #                 dict_data[str(i)] = eval(content[i])
+    #             except:
+    #                 dict_data[str(i)] = content[i]
+    #         param_val = self.get_param_reponse(param, dict_data)
+    #     if param_val is None:
+    #         return param_val
+    #     else:
+    #         if "$" + param == param_val:
+    #             param_val = None
+    #         return param_val
 
-    def get_param_reponse(self, param_name, dict_data, default=None):
-        for k, v in dict_data.items():
-            if k == param_name:
-                return v
-            else:
-                if isinstance(v, dict):
-                    ret = self.get_param_reponse(param_name, v)
-                    if ret is not default:
-                        return ret
-                if isinstance(v, list):
-                    for i in v:
-                        if isinstance(i, dict):
-                            ret = self.get_param_reponse(param_name, i)
-                            if ret is not default:
-                                return ret
-                        else:
-                            pass
-        return default
+    # def get_param_reponse(self, param_name, dict_data, default=None):
+    #     for k, v in dict_data.items():
+    #         if k == param_name:
+    #             return v
+    #         else:
+    #             if isinstance(v, dict):
+    #                 ret = self.get_param_reponse(param_name, v)
+    #                 if ret is not default:
+    #                     return ret
+    #             if isinstance(v, list):
+    #                 for i in v:
+    #                     if isinstance(i, dict):
+    #                         ret = self.get_param_reponse(param_name, i)
+    #                         if ret is not default:
+    #                             return ret
+    #                     else:
+    #                         pass
+    #     return default
 
     # 获取测试环境
     def get_env(self, env_id):
-        env = Master.objects.get(env_id=env_id)
-        prj_id = env.project.prj_id
-        return prj_id, env.url, env.private_key
+        master = Master.objects.get(id=env_id)
+        return master.name, master.url
 
     # # 获取签名方式
     # def get_sign(self, prj_id):
